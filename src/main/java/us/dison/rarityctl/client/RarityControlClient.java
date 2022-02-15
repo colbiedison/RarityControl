e24047437026b1c4fcca04693e8794fc2da2a725
Colbie Dison <cdison@protonmail.com>
2022-02-15 13:35:59 -0600

    Initial commit

---
+++ src/main/java/us/dison/rarityctl/client/RarityControlClient.java
@@ -0,0 +1,9 @@
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class RarityControlClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
